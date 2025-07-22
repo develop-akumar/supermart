4de6b8688c22cc00c6ff53aab0bcd46e5304972a
Arvind Chaurasia <develop.akumar@gmail.com>
2025-07-23 00:59:58 +0530

    Header comp | Search box css

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    FontAwesomeModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+  faSearch = faSearch;
+}
