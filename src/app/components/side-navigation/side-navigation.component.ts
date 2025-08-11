8286876323e139937f027936f242d738e110784f
Arvind Chaurasia <develop.akumar@gmail.com>
2025-08-12 00:23:29 +0530

    Side Navigation Bar | dummy categories

--- src/app/components/side-navigation/side-navigation.component.ts
+++ src/app/components/side-navigation/side-navigation.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-side-navigation',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './side-navigation.component.html',
   styleUrl: './side-navigation.component.css'
 })
 export class SideNavigationComponent {
 
+  faAngleDown = faAngleDown;
 }
