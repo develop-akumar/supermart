002efd155f25b170d1aab80e7752e32275ad328f
Arvind Chaurasia <develop.akumar@gmail.com>
2025-08-11 18:15:58 +0530

    Add 404 Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header.component';
-import { CatNavigationComponent } from "./components/cat-navigation/cat-navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    CatNavigationComponent
-],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'supermart';
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../header/header.component";
+import { CatNavigationComponent } from "../cat-navigation/cat-navigation.component";
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, CatNavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
