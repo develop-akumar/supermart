e7ae48bf005bc27ab88aea1d53aa19837c2a9110
Arvind Chaurasia <develop.akumar@gmail.com>
2025-08-11 16:48:55 +0530

    Implemented NgRx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,24 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+
+// NgRx
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+
+    provideStore({}),
+    provideEffects(),
+    provideStoreDevtools({}),
+  ],
 };
