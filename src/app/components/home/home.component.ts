954a7749475af37852239083f2aa7b9af0ec4de1
Arvind Chaurasia <develop.akumar@gmail.com>
2025-08-12 00:02:52 +0530

    Sidebar component part 1

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import { CatNavigationComponent } from "../cat-navigation/cat-navigation.component";
+import { SideNavigationComponent } from "../side-navigation/side-navigation.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CatNavigationComponent],
+  imports: [HeaderComponent, CatNavigationComponent, SideNavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
