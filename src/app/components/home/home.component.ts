e0c20ad5c642e12655ff73f5f95943b3ac9f78f3
Arvind Chaurasia <develop.akumar@gmail.com>
2025-08-13 02:52:52 +0530

    Make products comp and add images folder

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
-import { CatNavigationComponent } from "../cat-navigation/cat-navigation.component";
-import { SideNavigationComponent } from "../side-navigation/side-navigation.component";
+import { HeaderComponent } from '../header/header.component';
+import { CatNavigationComponent } from '../cat-navigation/cat-navigation.component';
+import { SideNavigationComponent } from '../side-navigation/side-navigation.component';
+import { ProductsComponent } from '../products/products.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CatNavigationComponent, SideNavigationComponent],
+  imports: [
+    HeaderComponent,
+    CatNavigationComponent,
+    SideNavigationComponent,
+    ProductsComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
